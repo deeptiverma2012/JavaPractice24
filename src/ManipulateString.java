7a3c843cd1485b48ae18c7bb4b05f95935ef3345
deeptiverma2012 <2012deeptiv@gmail.com>
2024-06-12 15:04:22 -0500

    First commit for Java practice

---
+++ src/ManipulateString.java
@@ -0,0 +1,38 @@
+
+public class ManipulateString {
+
+	public static void main(String[] args) {
+		String S= "I love Java";
+		System.out.println("I love 'Java'");
+		System.out.println("I love \"Java\"");
+		System.out.println("\"I love Java\"");
+		
+		String domain = "Java|Python|Ruby|Jscript";
+		
+		String arr[]= domain.split("\\|");
+		System.out.println(arr[0]);
+		
+		String domain1 = "Java.Python.Ruby.Jscript";
+		String arr1[]= domain1.split("\\.");
+		System.out.println(arr1[0]);
+		
+		String lang = "xXHelloSeleniumxxXTestingxxXAutomationxXxxJava";
+		String lanArr[]= lang.split("xX");
+		System.out.println(lanArr[0]);//blank output
+		System.out.println(lanArr[1]);
+		System.out.println(lanArr[2]);
+		System.out.println(lanArr[3]);
+		System.out.println(lanArr[4]);
+		
+		String St = "This is my Life My life my rules";
+		System.out.println(St.charAt(5));
+		System.out.println(St.codePointAt(5));
+		System.out.println(St.indexOf('L'));
+		System.out.println(St.indexOf('m', 11));
+		System.out.println(St.indexOf('m',St.indexOf("fe")+1));
+		System.out.println(St.indexOf('e', St.indexOf("ru")));
+		
+		
+	}
+
+}
---
+++ src/StringManipulations.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+public class StringManipulations {
+
+	public static String reverseStringOrder(String str) {
+		
+		String newStr[]= str.split(" ");
+		System.out.println(Arrays.toString(newStr));
+		
+		String revStr = "";
+		for(int i = newStr.length-1;i>=0;i--)
+		{
+			revStr = revStr +" "+ newStr[i];
+		}
+		
+		System.out.println(revStr);
+		
+		return revStr;
+		
+		
+	}
+	
+	public static void revEntireString(String ip)
+	{
+		String newip = "";
+		for(int i=ip.length()-1; i>=0;i--)
+		{
+			newip = newip+ip.charAt(i);
+		}
+		System.out.println(newip);
+	}
+	
+	public static void revEntireWord(String ip)
+	{
+		String st[] = ip.split(" ");
+		String newip = "";
+		
+		for(int i= 0; i<=st.length-1;i++)
+		{
+			StringBuilder sb = new StringBuilder(st[i]);
+			newip=newip+" "+sb.reverse();
+		}
+		
+		System.out.println(newip);
+	}
+	public static void main(String[] args) {
+		
+
+//		String S="I am the best to Best the am I";
+//		
+//		String reversedString = reverseStringOrder(S);
+		
+	String ip = "Selenium Automation is good";
+//		revEntireString(ip);
+	revEntireWord(ip);
+		
+	}
+
+}
